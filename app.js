5a9d20f94358d9616df533ff5225fadf71dc7394
sohadQtaitat <qtaitat3@gmail.com>
2019-10-15 17:28:19 +0300

    answer pro 1

--- app.js
+++ app.js
@@ -8,12 +8,16 @@ Write a function called sum() that takes in two numbers as arguments and then re
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSum() function below and check the console to see if the test passes.*/
 
 // Write your code here
-function sum(a, b) { //eslint-disable-line
-
+function sum(a, b) {
+    //give 4,7 should return [11,'The sum of 4 and 7 is 11']
+    var c = a + b;
+    var resultString = 'the sum of ' + a + ' and' + b + ' is' + c + ' .';
+    return [c, resultString];
 }
 
 // Here is the test for sum(); uncomment it to run it
-// testSum(4, 7);
+
+testSum(4, 7);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
